68347fcff7bf18cc119fd488c0dba5489901cbfb
Celia Leijten <celialeijten@gmail.com>
2021-12-10 15:23:54 +0100

    redux updated

--- learn-redux/learn-redux/src/App.js
+++ learn-redux/learn-redux/src/App.js
@@ -11,11 +11,12 @@ function App() {
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment(5))}>+</button> //increment by 5, geef 5 als argument mee
-      <button onClick={() => dispatch(decrement)}> -</button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}> minus</button>
       {isLogged ? <p>valuable information i should not see</p> : ''}
     </div>
   );
 }
 
 export default App;
+ //increment by 5, geef 5 als argument mee
